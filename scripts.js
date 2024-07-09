f16961f97962f91b56baa355a70f9762b4857f7a
Behshad <82580731+biigii@users.noreply.github.com>
2024-07-09 21:06:07 +1200

    javascript

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// alert("javascript");
+// console.log(10*10);
+console.log("10" + "10");
+console.log("hello" + " " + "world");
+
+console.log("true");
+console.log(true);
+
+// string  "hello world" "john" "123"
+// number  100, 1
+// boolean  true or false
+// array/list is a series of things [1,2,3] ["apple", "orange", "kiwi"]
+
+// variable
+
+var color = 1000;
+console.log(color);
+console.log(color * color);
+
+var name = "john";
+var surname = "smith";
+
+var fullName = name + " " + surname;
+// camel casing
+var backgroundColor = "#f11";
+var telephoneNumber = 021123456;
+var myOwnVariable = true;
+
+// kebab casing my-own-variable
+// snake casing my_own_variable
+// title casing MyOwnVariable
+
+console.log(fullName);
+
